9cead1b77ab31f639198f6b8c50d5bd771b7e6ff
0xJiro <jiro@sushi.com>
2023-04-03 00:03:27 -0400

    added incentives list logic w/ subgraph request

--- src/components/IncentivesList.tsx
+++ src/components/IncentivesList.tsx
@@ -1,13 +1,67 @@
 import { useAccount } from 'wagmi'
 import { useState } from 'react'
 import styled from 'styled-components'
+import { request, gql } from 'graphql-request'
+import { useQuery } from 'react-query'
+
+const endpoint = "https://api.thegraph.com/subgraphs/name/jiro-ono/v3-staker"
+  const INCENTIVES_QUERY = gql`
+    query {
+      incentives(first: 5) {
+        id
+        staker {
+          id
+        }
+        creator {
+          id
+        }
+        rewardToken
+        pool
+        startTime
+        endTime
+        vestingPeriod
+        refundee
+        initialRewardAmount
+		    ended
+        stakes {
+          id
+        }
+      }
+    }
+  `
 
 export function IncentivesList() {
   const { address, isConnected } = useAccount()
-  
+
+  const { data, isLoading, error } = useQuery("incentives", () => {
+    return request(endpoint, INCENTIVES_QUERY)
+  })
+
   return (
     <div>
       <h2>Incentives List</h2>
+      {isLoading && <p>Loading...</p>}
+        {data != undefined && 
+          <div>
+            {data.incentives.map((incentives) => (
+              <div key={incentives.id}>
+                <p>Incentive Id: {incentives.id}</p>
+                <ul>
+                  <li>Staker: {incentives.staker.id}</li>
+                  <li>Creator: {incentives.creator.id}</li>
+                  <li>Reward Token: {incentives.rewardToken}</li>
+                  <li>Pool: {incentives.pool}</li>
+                  <li>Start Time: {incentives.startTime}</li>
+                  <li>End Time: {incentives.endTime}</li>
+                  <li>Vesting Period: {incentives.vestingPeriod}</li>
+                  <li>Refundee: {incentives.refundee}</li>
+                  <li>Initial Reward Amount: {incentives.initialRewardAmount}</li>
+                  <li>Ended: {incentives.ended.toString()}</li>
+                </ul>
+              </div>
+            ))}
+          </div>
+        }
     </div>
   )
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,20 +2,26 @@ import type { AppProps } from 'next/app'
 import NextHead from 'next/head'
 import * as React from 'react'
 import { WagmiConfig } from 'wagmi'
+import { QueryClient, QueryClientProvider } from "react-query"
 
 import { client } from '../wagmi'
 
 function App({ Component, pageProps }: AppProps) {
   const [mounted, setMounted] = React.useState(false)
+
+  const queryClient = new QueryClient()
+
   React.useEffect(() => setMounted(true), [])
   return (
-    <WagmiConfig client={client}>
-      <NextHead>
-        <title>wagmi</title>
-      </NextHead>
+    <QueryClientProvider client={queryClient}>
+      <WagmiConfig client={client}>
+        <NextHead>
+          <title>wagmi</title>
+        </NextHead>
 
-      {mounted && <Component {...pageProps} />}
-    </WagmiConfig>
+        {mounted && <Component {...pageProps} />}
+      </WagmiConfig>
+    </QueryClientProvider>
   )
 }
 
